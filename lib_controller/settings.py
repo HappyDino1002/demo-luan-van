c247963eaeb6de5da55b0734bf3da7da7e4cb516
trunghieule0301 <63578252+trunghieule0301@users.noreply.github.com>
2021-08-13 20:28:47 +0700

    Update settings.py

--- lib_controller/settings.py
+++ lib_controller/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import dj_database_url
 import os
 from pathlib import Path
 
@@ -104,9 +103,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
